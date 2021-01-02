bc34425b61d30d6cf391b3b7644a0d7c7dbefd37
Royce <royce.com@gmail.com>
2021-01-02 19:26:56 +0800

    Improved spec template

--- {{cookiecutter.project_slug}}/test/solution.spec.js
+++ {{cookiecutter.project_slug}}/test/solution.spec.js
@@ -5,13 +5,25 @@ import { Solution } from '../src/solution'
 
 describe('Solution', () => {
   describe('.solution', () => {
-    const inputData = []
-    const expectedData = []
+    const dataTable = [
+      { input: 1, expected: 1 },
+    ]
 
-    for (let i = 0; i < inputData.length; i += 1) {
-      describe(`when given ${inputData[i]}`, () => {
+    // Items to check as needed:
+    // Check efficiency as required
+    // Maximum
+    // Minimum
+    // Odd
+    // Even
+    // Negative numbers
+    // Load Test
+
+    for (let i = 0; i < dataTable.length; i += 1) {
+      describe(`when given ${dataTable[i].input}`, () => {
         it('passes', () => {
-          expect(Solution.solution()).to.eq(expectedData[i])
+          expect(Solution.solution(dataTable[i].input)).to.eq(
+            expectedData[i].expected
+          )
         })
       })
     }
